461f0ecb77c152bb928510c0e9b13360ac627d32
Cauã Henrique <92392798+caffe0@users.noreply.github.com>
2024-02-15 23:58:47 -0300

    Update DesafioDNS.py

--- DesafioDNS.py
+++ DesafioDNS.py
@@ -1,10 +1,11 @@
+#ESTE CÓDIGO SERVE PARA SIMULAR UMA LIMPEZA DA MEMÓRIA MATRIZ DE UM SERVIDOR E DA MEMÓRIA CACHE DO HISÓRICO DE LINKS URLS UTILIZADAS PELO USUÁRIO#
 n = int(input('quantos servidores voce vai entrar\n')) #quantidade de urls
 k = int(input('qual o tempo de resposta do servidor?\n'))
 m = int(input('qual o tempo de resposta da máquina?\n'))
 cacheDns = int(input('qual a memória do servidor dns?\n'))
 url = []
 for i in range(n):
-    urlNv = input()
+    urlNv = input('Digite a url que você deseja entrar: \n')
     if len(url)<=cacheDns:
         if urlNv in url:
             indexNovo = url.index(urlNv)
